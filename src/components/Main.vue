61671758903e5563977373062e29103fe71eb187
codesboy <565195693@qq.com>
2018-03-08 19:56:46 +0800

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,125 +2,207 @@
     <div id="app">
         <h1 v-if="!beginLucky">抽奖尚未开始或者已经结束</h1>
         <div v-else>
-            <h2>抽奖</h2>
-            {{name}}--{{phone}}
-            <div id='div' style='width:540px;min-height:360px;background:url("http://www.thinkphp.cn/Uploads/editor/2016-07-08/577f852e40237.jpg") no-repeat'></div>
+            <h2>贝臣刮刮乐</h2>
+            <!-- {{name}}--{{phone}} -->
+            <div class="card">
+                <canvas id="canvas" width="280px" height="100px"></canvas>
+                <div class="lucky">{{lucky}}</div>
+            </div>
+            <div class="msg">{{msg}}</div>
         </div>
     </div>
 </template>
 
 <script>
-function init() {
-    if (!document.getElementById("myCanvas")) {
-        var width = "";
-        var height = "";
-        var canvas = document.createElement("canvas");
-        // width = document.getElementById("div").offsetWidth;
-        width = 540;
-        // height = document.getElementById("div").offsetHeight;
-        height = 300;
-        canvas.setAttribute("width", width + "px");
-        canvas.setAttribute("height", height + "px");
-        canvas.setAttribute("style", "border:1px solid green");
-        canvas.id = "myCanvas";
-        document.getElementById("div").appendChild(canvas);
-    }
-
-    var myCanvasObject = document.getElementById("myCanvas");
+var draw;
+var preHandler = function(e) {
+  e.preventDefault();
+};
+import cardImg from "../assets/card.jpg";
+class Draw {
+  init(id,vue) {
+    var width = 280;
+    var height = 100;
+    var myCanvasObject = document.getElementById(id);
     var ctx = myCanvasObject.getContext("2d");
 
-    //绘制黑色矩形    
-    ctx.beginPath();
-    ctx.fillStyle = "#939393";
-    ctx.rect(0, 0, width, height);
-    ctx.closePath();
-    ctx.fill();
+    //绘制黑色矩形
+    // ctx.beginPath();
+    // ctx.fillStyle = "#939393";
+    // ctx.rect(0, 0, width, height);
+    // ctx.closePath();
+    // ctx.fill();
+
+    var img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+    };
+    // img.src = "https://bcwx.rehack.cn/static/lucky/images/card.jpg";
+    img.src = cardImg;
 
     var isDown = false; //鼠标是否按下标志
     var pointerArr = []; //鼠标移动坐标数组
-    var xPointer = 0;//鼠标当前x坐标
-    var yPointer = 0;//鼠标当前y坐标
-
+    var xPointer = 0; //鼠标当前x坐标
+    var yPointer = 0; //鼠标当前y坐标
 
     //pc，移动事件兼容写法
     var hastouch = "ontouchstart" in window ? true : false,
-    tapstart = hastouch ? "touchstart" : "mousedown",
-    tapmove = hastouch ? "touchmove" : "mousemove",
-    tapend = hastouch ? "touchend" : "mouseup";
-
+      tapstart = hastouch ? "touchstart" : "mousedown",
+      tapmove = hastouch ? "touchmove" : "mousemove",
+      tapend = hastouch ? "touchend" : "mouseup";
 
     //鼠标按下
     myCanvasObject.addEventListener(tapstart, function(event) {
-        var e=window.event||event;//2017-12-06
-        this.style.cursor = "move";
-        isDown = true;
-        xPointer = hastouch ? e.targetTouches[0].pageX : e.clientX - this.offsetLeft;
-        yPointer = hastouch ? e.targetTouches[0].pageY : e.clientY - this.offsetTop;;
-        pointerArr.push([xPointer, yPointer]);
-        circleReset(ctx);
-    });
+      //   console.log("按下");
+      var e = window.event || event; //2017-12-06
+      this.style.cursor = "move";
+      isDown = true;
+      xPointer = hastouch
+        ? e.targetTouches[0].pageX - myCanvasObject.getBoundingClientRect().left
+        : e.clientX - this.offsetLeft;
+      yPointer = hastouch
+        ? e.targetTouches[0].pageY - myCanvasObject.getBoundingClientRect().top
+        : e.clientY - this.offsetTop;
 
+      console.log(xPointer, yPointer);
+      pointerArr.push([xPointer, yPointer]);
+      circleReset(ctx);
+    });
 
     //鼠标按下后拖动
     myCanvasObject.addEventListener(tapmove, function(event) {
-        var e=window.event||event;//2017-12-06
-        if (isDown) {
-        xPointer = hastouch ? e.targetTouches[0].pageX : e.clientX - this.offsetLeft;;
-        yPointer = hastouch ? e.targetTouches[0].pageY : e.clientY - this.offsetTop;;
+      // console.log("拖动");
+      var e = window.event || event; //2017-12-06
+      if (isDown) {
+        xPointer = hastouch
+          ? e.targetTouches[0].pageX -
+            myCanvasObject.getBoundingClientRect().left
+          : e.clientX - this.offsetLeft;
+        yPointer = hastouch
+          ? e.targetTouches[0].pageY -
+            myCanvasObject.getBoundingClientRect().top
+          : e.clientY - this.offsetTop;
         pointerArr.push([xPointer, yPointer]);
         circleReset(ctx);
-        }
+      }
     });
-
 
     //鼠标抬起取消事件
     myCanvasObject.addEventListener(tapend, function(event) {
-        isDown = false;
-        pointerArr = [];
+      isDown = false;
+      pointerArr = [];
+      var data = ctx.getImageData(
+        0,
+        0,
+        myCanvasObject.width,
+        myCanvasObject.height
+      ).data,scrapeNum =0;
+    //   console.log(a);
+      for (var i = 3, len = data.length; i < len; i += 4) {
+        if (data[i] === 0) {
+          scrapeNum++;
+        }
+      }
+      if (scrapeNum > myCanvasObject.width* myCanvasObject.height * 0.2) {
+          if(vue.lucky!='谢谢惠顾!'){
+
+            vue.msg=`恭喜你中奖:${vue.lucky}`;
+          }else{
+              vue.msg=`很抱歉，您未中奖！`;
+          }
+        
+      }
+      if(scrapeNum > myCanvasObject.width* myCanvasObject.height * 0.5){
+        ctx.clearRect(0, 0, myCanvasObject.width, myCanvasObject.height);
+
+      }
     });
 
-
     //圆形橡皮檫
-    function circleReset(ctx) {
-        ctx.save();
-        ctx.beginPath();
-        ctx.moveTo(pointerArr[0][0], pointerArr[0][1]);
-        ctx.lineCap = "round";　　 //设置线条两端为圆弧
-        ctx.lineJoin = "round";　　 //设置线条转折为圆弧
-        ctx.lineWidth = 60;
-        ctx.globalCompositeOperation = "destination-out";
-        if (pointerArr.length == 1) {
-            ctx.lineTo(pointerArr[0][0] + 1, pointerArr[0][1] + 1);
-        } else {
-            for (var i=1;i<pointerArr.length;i++) {
-                ctx.lineTo(pointerArr[i][0], pointerArr[i][1]);
-                ctx.moveTo(pointerArr[i][0], pointerArr[i][1]);
-            }
+    var circleReset = function(ctx) {
+      // console.log('檫')
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(pointerArr[0][0], pointerArr[0][1]);
+      ctx.lineCap = "round"; //设置线条两端为圆弧
+      ctx.lineJoin = "round"; //设置线条转折为圆弧
+      ctx.lineWidth = 20; //画笔大小
+      ctx.globalCompositeOperation = "destination-out";
+      if (pointerArr.length == 1) {
+        ctx.lineTo(pointerArr[0][0] + 1, pointerArr[0][1] + 1);
+      } else {
+        for (var i = 1; i < pointerArr.length; i++) {
+          ctx.lineTo(pointerArr[i][0], pointerArr[i][1]);
+          ctx.moveTo(pointerArr[i][0], pointerArr[i][1]);
         }
-        ctx.closePath();
-        ctx.stroke();
-        ctx.restore();
-    }
-}
-init()
-
-
-export default {
-  data(){
-      return {  
-          beginLucky:true,
-          name:'',
-          phone:''
-      }  
-  },
-  created(){
-      let st = window.localStorage;
-      this.name=st.getItem('name');
-      this.phone=st.getItem('phone');
+      }
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+    };
   }
 }
+
+export default {
+  data() {
+    return {
+      beginLucky: true,
+      phone: "",
+      msg:'',
+      lucky:null
+    };
+  },
+  mounted() {
+    draw = new Draw();
+    draw.init("canvas",this);
+    if(window.localStorage.getItem('lucky')){
+        this.lucky=window.localStorage.getItem('lucky');
+    }else{
+        this.lucky='谢谢惠顾!';
+    }
+  },
+  created() {
+    let st = window.localStorage;
+    if (st.getItem("phone")) {
+      this.phone = st.getItem("phone");
+    } else {
+      this.$router.push({ path: "/" });
+    }
+  }
+};
 </script>
 
-<style scoped>
+<style >
+@import "../assets/base.css";
+.card {
+  position: relative;
+  width: 280px;
+  height: 100px;
+  margin: 20px auto;
 
+}
+h2 {
+  text-align: center;
+
+}
+.card .lucky {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  line-height: 100px;
+  z-index: 0;
+  font-size: 22px;
+  border:1px solid #ccc;
+}
+
+#canvas {
+  /* background: red; */
+  position: absolute;
+  z-index: 1;
+
+}
+.msg{
+    color: red;
+    text-align: center;
+}
 </style>
--- src/components/Reg.vue
+++ src/components/Reg.vue
@@ -1,61 +1,89 @@
 <template>
-	<div id="app">
+	<div id="app" class="main">
         <h2>报名刮奖</h2>
-        <div>
-            <label>姓名</label>
-            <p><input placeholder="请输入姓名" v-model="name"/></p>
-        </div>
-        <div>
+        <div class="item">
             <label>手机号</label>
             <p><input placeholder="请输入手机号" v-model="phone"/></p>
         </div>
-        <button @click="isLogin">立即报名</button> 
+        <div class="btn" @click="isLogin">立即参与</div> 
+        <div class="notice">为了保证您顺利进行刮奖，请务必正确填写您的资料,该资料将作为您的兑奖凭证!</div>
         <div>{{errmsg}}</div>
     </div>
 	</template>
 	<script>
-        import axios from "axios";
-		export default {
-			data() {
-				return {
-					name:'',
-                    phone:'',
-                    errmsg:'',
-                    issignup:1
-				}
-			},
-			created() {
-				if (window.localStorage.getItem("issignup") == "2") {
-                    this.$router.push({ path: 'main' });
-                    return false;
-                }
-			},
-			methods:{
-				isLogin:function() {
-                    axios
-                    .post("https://bcwx.rehack.cn/api/v1/signup", {
-                        'name':this.name,
-                        'phone':this.phone
-                    })
-                    .then(response => {
-                        if (response.data.phone==this.phone) {
-                            alert('报名成功')
-                            this.$router.push({ path: 'main' });
-                            window.localStorage.setItem("name", this.name);//本地存储标记已经报名
-                            window.localStorage.setItem("phone", this.phone);//本地存储标记已经报名
-                        }else{
-                            alert('未知错误')
-                        }
-                    })
-                    .catch(err=>{
-                        alert('发生错误')
-                        this.errmsg=err.response.data
-                    });
-					
-				}
-			}
-		}
-	</script>
-	<style>
-		
-	</style>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      name: "",
+      phone: "",
+      errmsg: "",
+      issignup: 1
+    };
+  },
+  created() {
+    if (window.localStorage.getItem("issignup") == "2") {
+      this.$router.push({ path: "main" });
+      return false;
+    }
+  },
+  methods: {
+    isLogin: function() {
+      axios
+        .post("https://bcwx.rehack.cn/api/v1/signup", {
+          phone: this.phone
+        })
+        .then(response => {
+          if (response.data.phone == this.phone) {
+            
+            this.$router.push({ path: "main" });
+            window.localStorage.setItem("phone", this.phone); //本地存储标记已经报名
+            window.localStorage.setItem("lucky", response.data.lucky_item); //本地存储标记已经报名
+          } else {
+            alert("未知错误");
+          }
+        })
+        .catch(err => {
+          alert("抱歉，您没有参与本次刮奖的权限，请咨询65006500");
+        //   this.errmsg = err.response.data;
+        //   if(err.response.data.msg.phone=='phone已存在'){
+        //       window.localStorage.setItem("name", this.name); //本地存储标记已经报名
+        //     window.localStorage.setItem("phone", this.phone); //本地存储标记已经报名
+        //       this.$router.push({ path: "main" });
+        //   }
+        });
+    }
+  }
+};
+</script>
+<style>
+@import url('../assets/base.css');
+.main {
+    width: 80%;
+  margin: 0 auto;
+}
+.item{
+    margin: 10px 0;
+}
+input{
+    height: 30px;
+    padding-left: 6px;
+    outline: none;
+    border:1px solid #ccc;
+    border-radius: 6px;
+    -webkit-appearance: none;
+}
+.btn{
+    width: 100px;
+    height: 35px;
+    line-height: 35px;
+    color: #fff;
+    text-align: center;
+    border-radius: 4px;
+    background: rgb(233, 104, 107);
+}
+.notice{
+    font-size: 12px;
+    color: red;
+}
+</style>
